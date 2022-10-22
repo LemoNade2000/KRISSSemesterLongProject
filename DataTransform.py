e05439c9f620d706eaa837c76b7450af227ada65
LemoNade2000 <nitroped7@gmail.com>
2022-10-23 02:41:59 +0800

    Completed Data Transform.

--- DataTransform.py
+++ DataTransform.py
@@ -9,25 +9,39 @@ rootdir = os.getcwd()
 
 def doTransform(filepath, filename):
     dir = rootdir + "\\Data\\TransformedData\\" + filename
-    # df = csv.reader(filepath)
-    df = pd.DataFrame([[5.0, 1.0, 2.0, 4.0, 3.0], [6.0, 7.0, 2.0, 5.0, 3.0]])
+    df = pd.read_csv(filepath)
+    df = df.set_index('Unnamed: 0')
+    df= df.rename_axis(None, axis=1).rename_axis('id', axis=0)
+    df = df.apply(pd.to_numeric, errors = 'coerce')
+    df = df.dropna(axis = 0, how = 'any')
     df_edit = df.copy()
-    df['RecoveryDate'] = df_edit.idxmin(axis = 1)
+    df['RecoveryDate'] = df_edit.idxmin(axis = 1).astype(int)
     df_edit['min'] = df_edit.min(axis = 1)
-
+    loopNumber = 0
     # Get the maximum after the minimum has been reachesd
     for index, row in df_edit.iterrows(): 
-        while(row.idxmax(axis = 0) < row.idxmin(axis = 0)):
-            row[row.idxmax(axis = 0)] = 0
+        max = int(row.idxmax())
+        min = int(row.idxmin())
+        while(max < min):
+            row[row.idxmax()] = 0
+            max = int(row.idxmax())
+        df_edit.iloc[loopNumber] = row
+        loopNumber += 1
 
-    df['RecoveryFinishDate'] = df_edit.max(axis = 1)
-    df['RecoveryMagnitude'] = 100 * (df_edit.max(axis = 1) - df_edit.min(axis = 1)) / df_edit['min'] # Percentage change from min to max
-    df['RecoveryTerm'] = (df['RecoveryFinishDate'] - df['RecoveryDate']).astype(int)
+    df['RecoveryFDate'] = df_edit.idxmax(axis = 1).astype(int)
+    df['RecoveryMagnitude'] = 100 * (df_edit.max(axis = 1) - df_edit['min']) / df_edit['min'] # Percentage change from min to max
+    df['RecoveryTerm'] = (df['RecoveryFDate'] - df['RecoveryDate']).astype(int)
     df.to_csv(dir)
     return 
 
 if __name__ == '__main__':
     datadir = rootdir + "\\Data\\"
+    fileNum = 0
     for filename in os.scandir(datadir):
+        print(fileNum)
         if filename.is_file():
-            doTransform(filename.path, filename.name)
+            try:
+                doTransform(filename.path, filename.name)
+            except:
+                continue
+        fileNum += 1
