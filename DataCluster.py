c4c6724eb50f89fd138fe109c91a202e87d4b560
LemoNade2000 <nitroped7@gmail.com>
2022-11-29 20:54:41 +0800

    backtest complete

--- DataCluster.py
+++ DataCluster.py
@@ -27,6 +27,8 @@ def vectorizeCSV():
 
 def main():
     df, termDF = vectorizeCSV()
+    df = df.drop(columns = np.arange(30, 34), axis = 1)
+    termDF = termDF.drop(columns = np.arange(30, 34), axis = 1)
     df_reduced = FeatureAgglomeration(n_clusters=2)
     df_reduced.fit_transform(df)
     featureLabel = df_reduced.labels_
@@ -46,6 +48,8 @@ def main():
     label_1 = df_reduced[labels == 0]
     label_2 = df_reduced[labels == 1]
     df_reduced['MeanRecoveryTerm'] = termDF.loc[:,featureLabel == 0].mean(axis = 1)
+    df_reduced['AltMeanRecoveryTerm'] = termDF.loc[:,featureLabel == 1].mean(axis = 1)
+    df_reduced['Cluster'] = labels
     df_reduced.to_csv("TermInformation.csv")
     sns.scatterplot(data = label_0, x = label_0[0], y = label_0[1], color = 'black')
     sns.scatterplot(data = label_1, x = label_1[0], y = label_1[1], color = 'red')
@@ -56,8 +60,43 @@ def main():
     print(df.index[labels == 0])
     print(df.index[labels == 1])
     print(cluster.labels_)
-    return 0
+    return df.index[labels == 0]
 
-if __name__ == "__main__":
-    main()
+def retroactive():
+    df, termDF = vectorizeCSV()
+    df_reduced = FeatureAgglomeration(n_clusters=2)
+    df_reduced.fit_transform(df)
+    featureLabel = df_reduced.labels_
+    print(df_reduced.labels_)
+    plot_df = df_reduced.transform(df)
+    df_reduced = df_reduced.transform(df)
+    df_reduced = pd.DataFrame(df_reduced)
+    df_reduced.index = df.index
+    print(df_reduced)
+    fig = plt.figure(figsize =(10, 7))
+    plt.boxplot(plot_df)
+    plt.savefig('Feature1Retro.png')
+    plt.clf()
+    cluster = OPTICS(min_cluster_size=0.1).fit(df_reduced)
+    labels = cluster.labels_
+    label_0 = df_reduced[labels == -1]
+    label_1 = df_reduced[labels == 0]
+    label_2 = df_reduced[labels == 1]
+    df_reduced['MeanRecoveryTerm'] = termDF.loc[:,featureLabel == 0].mean(axis = 1)
+    df_reduced['AltMeanRecoveryTerm'] = termDF.loc[:,featureLabel == 1].mean(axis = 1)
+    df_reduced['Cluster'] = labels
+    df_reduced.to_csv("TermInformationRetroactive.csv")
+    sns.scatterplot(data = label_0, x = label_0[0], y = label_0[1], color = 'black')
+    sns.scatterplot(data = label_1, x = label_1[0], y = label_1[1], color = 'red')
+    sns.scatterplot(data = label_2, x = label_2[0], y = label_2[1], color = 'blue')
+    plt.xlim([0, 30])
+    plt.ylim([0, 30])
+    plt.savefig('ScatterplotRetroactive.png')
+    print(df.index[labels == 0])
+    print(df.index[labels == 1])
+    print(cluster.labels_)
+    return df.index[labels == 0]
+
+main()
+# retroactive()
 
